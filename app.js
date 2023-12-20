77e7e2fd5361bcf796c360a173b85818be87385a
Mouaadgames <mouaadgames7@gmail.com>
2023-12-20 09:39:21 +0100

    adding not found page

--- app.js
+++ app.js
@@ -4,8 +4,12 @@ const port = process.env.PORT ?? 3000;
 
 app.use(express.static('public'))
 
+app.get("/",(req,res)=>{
+    res.redirect("/")
+})
+
 app.get('*', (req, res) => {
-    res.redirect('/');
+    res.redirect('/not-found');
 })
 
 app.listen(port, () => {
